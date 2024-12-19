b8b769b9376c79b91f70af6a57ecb24b9ce6204f
DA-ARYAA <deepesh.arya@algoscale.com>
2024-12-19 11:32:54 +0530

    completed kanban

--- src/store/taskStore.js
+++ src/store/taskStore.js
@@ -1,18 +1,6 @@
 import { createStore } from "redux";
 
 export default () => {
-  const store = createStore((state = { tasks: [] }, action) => {
-    // console.log({ state, action });
-    // switch (action.type) {
-    //   case "to_do":
-    //     return { counter: state.counter + 1 };
-    //   case "in_progress":
-    //     return { counter: state.counter - 1 };
-    //   case "done":
-    //     return { counter: state.counter - 1 };
-    //   default:
-    //     return state;
-    // }
-  });
+  const store = createStore((state = { tasks: [] }, action) => {});
   return store;
 };
